15ba3c027ea0441dc067713b2821eff3388b7d49
Changwoon Choi <changwoon.choi00@gmail.com>
2023-03-05 21:39:11 +0900

    start implementing publications

--- components/grid-item.js
+++ components/grid-item.js
@@ -45,6 +45,30 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => (
   </Box>
 )
 
+export const PubGridItem = ({ children, id, title, thumbnail }) => (
+  <Box w="100%" textAlign="center">
+    <LinkBox
+      as={NextLink}
+      href={`/works/${id}`}
+      scroll={false}
+      cursor="pointer"
+    >
+      <Image
+        src={thumbnail}
+        alt={title}
+        className="grid-item-thumbnail"
+        placeholder="blur"
+      />
+      <LinkOverlay as="div" href={`/works/${id}`}>
+        <Text mt={2} fontSize={20}>
+          {title}
+        </Text>
+      </LinkOverlay>
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
+  </Box>
+)
+
 export const GridItemStyle = () => (
   <Global
     styles={`

443f2ea1ff565442fd23092fb2e2d37c6e7c92cb
Changwoon Choi <changwoon.choi00@gmail.com>
2023-03-09 00:11:14 +0900

    remove temporarily unused features

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
+// import VoxelDogLoader from '../voxel-dog-loader'
 
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+// const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+//   ssr: false,
+//   loading: () => <VoxelDogLoader />
+// })
 // insert <LazyVoxelDog /> before children when you prepare 3D model.
 
 const Main = ({ children, router }) => {

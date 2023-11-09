f2cc81d98cb30227e5cb7fe9e9dd46c2f913b2f9
changwoonchoi <zzzmaster@snu.ac.kr>
2023-11-08 17:38:57 -0800

    add naver cloud

--- pages/index.js
+++ pages/index.js
@@ -169,11 +169,15 @@ const Home = () => (
           ROK Air Force (Operations Command)
         </BioSection>
         <BioSection>
-          <BioYear>2023.05 -</BioYear>
+          <BioYear>2023.05 - 2023.12</BioYear>
           Visiting Graduate Student, University of California, San Diego, advised by {' '}
           <Link as={NextLink} href="https://cseweb.ucsd.edu/~haosu/" passHref scroll={false}>
             Hao Su
           </Link>
+        </BioSection>
+        <BioSection>
+          <BioYear>2023.12 - </BioYear>
+          Research Scientist Intern, NAVER Cloud (CLOVA)
         </BioSection>
         <BioSection>
           <BioYear>Conference Reviewer</BioYear>
